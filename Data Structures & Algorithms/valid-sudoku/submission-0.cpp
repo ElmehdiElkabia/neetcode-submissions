18b4878c7bed12794a4b79fb0c638cc773659351
El mehdi Elkabia <149374411+ElmehdiElkabia@users.noreply.github.com>
2026-05-21 00:56:10 +0100

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+   public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<char> rows[9], cols[9], boxes[9];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                int c = board[i][j];
+                if (c == '.') continue;
+                int boxesize = (i / 3) * 3 + (j / 3);
+                if (rows[i].count(c) || cols[j].count(c) || boxes[boxesize].count(c)) return false;
+
+                rows[i].insert(c);
+                cols[j].insert(c);
+                boxes[boxesize].insert(c);
+            }
+        }
+        return true;
+    }
+};
